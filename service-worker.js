32eb4b070c6c4583892c49daf661465056291653
Anton Metlitsky <ticket1201@gmail.com>
2022-06-22 22:26:53 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simple-react-homeworks/precache-manifest.96c911fb5723cc75f05a7b15dfd9fbc3.js"
+  "/simple-react-homeworks/precache-manifest.d6e2f5ca50b465a9dfb3a0c48d598dd8.js"
 );
 
 self.addEventListener('message', (event) => {
