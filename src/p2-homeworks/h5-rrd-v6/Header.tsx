90089f02003f85543730b49301b967b77ade45f6
Anton Metlitsky <ticket1201@gmail.com>
2022-06-25 17:07:49 +0300

    feat: add ternary choose of class

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -7,9 +7,9 @@ function Header() {
     return (
         <div className={s.wrapper}>
            <div className={s.container}>
-               <NavLink className={s.links} to={'/pre-junior'}>PreJunior</NavLink>
-               <NavLink className={s.links} to={'/junior'}>Junior</NavLink>
-               <NavLink className={s.links} to={'/pre-junior-plus'}>Junior+</NavLink>
+               <NavLink className={({isActive})=> isActive ? `${s.links} ${s.active}` : `${s.links}`} to={'/pre-junior'}>PreJunior</NavLink>
+               <NavLink className={({isActive})=> isActive ? `${s.links} ${s.active}` : `${s.links}`} to={'/junior'}>Junior</NavLink>
+               <NavLink className={({isActive})=> isActive ? `${s.links} ${s.active}` : `${s.links}`} to={'/pre-junior-plus'}>Junior+</NavLink>
            </div>
            <div className={s.colorBox}></div>
         </div>
