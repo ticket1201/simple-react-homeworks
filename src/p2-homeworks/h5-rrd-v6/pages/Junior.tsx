69585d60bfa3793e51da3307e05af8037bafbecf
Anton Metlitsky <ticket1201@gmail.com>
2022-06-25 16:44:37 +0300

    feat: impl PreJuniorPlus, Junior components

---
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Junior = () => {
+    return (
+        <div>
+           Junior
+        </div>
+    );
+};
+
+export default Junior;
---
+++ src/p2-homeworks/h5-rrd-v6/pages/PreJuniorPlus.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PreJuniorPlus = () => {
+    return (
+        <div>
+            PreJuniorPlus
+        </div>
+    );
+};
+
+export default PreJuniorPlus;
