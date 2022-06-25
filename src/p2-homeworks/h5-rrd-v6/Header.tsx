15d0bc31fdc45c9dfafb7354dc8f26d06c014952
Anton Metlitsky <ticket1201@gmail.com>
2022-06-25 16:48:05 +0300

    feat: add NavLinks and colorbox, styles

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom';
+import s from './Header.module.css'
+
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.wrapper}>
+           <div className={s.container}>
+               <NavLink className={s.links} to={'/pre-junior'}>PreJunior</NavLink>
+               <NavLink className={s.links} to={'/junior'}>Junior</NavLink>
+               <NavLink className={s.links} to={'/pre-junior-plus'}>Junior+</NavLink>
+           </div>
+           <div className={s.colorBox}></div>
         </div>
     )
 }
