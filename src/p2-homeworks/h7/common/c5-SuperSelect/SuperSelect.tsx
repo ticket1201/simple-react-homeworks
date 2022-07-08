8f7318b70969d967b69c31fd97154a2424f34d94
Anton Metlitsky <ticket1201@gmail.com>
2022-07-08 12:44:00 +0300

    feat: impl onChangeCallback and mappedOptions functions

--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -1,4 +1,5 @@
 import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react'
+import s from './SuperSelect.module.css';
 
 type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
 
@@ -14,14 +15,26 @@ const SuperSelect: React.FC<SuperSelectPropsType> = (
         ...restProps
     }
 ) => {
-    const mappedOptions: any[] = []; // map options with key
+    const mappedOptions: any[] = options
+        ? options.map((el, index) => {
+            return (
+                <option key={index} className={s.option}>{el}</option>)
+        })
+        : [] // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+        let newValue = e.currentTarget.value
+        if (onChangeOption){
+            onChangeOption(newValue)
+        }
+        else if(!onChangeOption && onChange){
+            onChange(e)
+        }
         // onChange, onChangeOption
     }
 
     return (
-        <select onChange={onChangeCallback} {...restProps}>
+        <select onChange={onChangeCallback} {...restProps} className={s.select}>
             {mappedOptions}
         </select>
     )
