d3e636ddb6c12ba668e6e3e77f81a8b9d258ead4
Anton Metlitsky <ticket1201@gmail.com>
2022-07-13 12:21:24 +0300

    feat: impl homeWorkReducer

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,12 +1,24 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from '../HW8';
+
+type ActionType = {
+    type: string
+    payload: string | number
+}
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
+            if(action.payload === 'up'){
+                return [...state].sort((a,b) => a.name.localeCompare(b.name))
+            }
+            else if(action.payload === 'down'){
+                return [...state].sort( (a,b) => b.name.localeCompare(a.name) )
+            }
+
             return state
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(el => el.age >= action.payload)
         }
         default: return state
     }
